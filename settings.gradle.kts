aececc328e3f2a970c1f8be2e04b3c07d9bf252b
Faizalilham <faizalfalakh19@gmail.com>
2021-10-28 20:01:03 +0700

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "MyProjects1"
+
---
+++ src/main/kotlin/Week1.kt
@@ -0,0 +1,42 @@
+fun main() {
+    var kardus = "Buku"
+    kardus = "Pulpen"
+    println(kardus)
+//    val rumah= "kursi"
+//    rumah= "bangku"
+//    Bilangan Bulat
+    var byte :Byte = 120
+    var short: Short = 32_000
+    var int : Int = 100_000_000
+    var long : Long = 500_000_000
+
+//    Bilangan Desimal
+    var float  : Float = 3.14f
+    var double: Double = 3.111111114
+
+//    Tipe data Char
+    var char : Char = 'A'
+
+//    Tipe data String
+    var nama : String = "Amanda"
+    var alamat = """
+        Cetak Cetak Cetak Cetak Cetak Cetak
+        Cetak Cetak Cetak Cetak
+        Cetak CetakCetak
+    """.trimIndent()
+//    Tipe Bolean
+    var benar : Boolean = true
+    var salah : Boolean = false
+    println(byte)
+    println(short)
+    println(int)
+    println(long)
+    println(float)
+    println(double)
+    println(char)
+    println(nama)
+    println(alamat)
+    println(salah)
+    println(benar)
+
+}
